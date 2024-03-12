521de604dd9541b85ba3293c7c04853df6ab8ac1
CharistaSC <bryontian@gmail.com>
2024-03-13 01:06:56 +0800

    Extensive Training Push part 1

---
+++ Bryons test/model_train.py
@@ -0,0 +1,17 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Input data files are available in the read-only "../input/" directory
+# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
+
+import os
+for dirname, _, filenames in os.walk('input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+# You can write up to 5GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All" 
+# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
+        
+df = pd.read_csv('input/emails.csv')
