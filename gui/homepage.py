954baca196da0810e9645b6a7d2e3998b0785fa7
Valerie <aeginana@gmail.com>
2024-03-14 11:37:45 +0800

    Major cleanup I

--- gui/homepage.py
+++ gui/homepage.py
@@ -1,11 +1,12 @@
+
 import streamlit as st
 import os
 from datasets import load_dataset
 # Define your pages
 pages = {
     "💯 Home": "homepage.py",
-    "📥 Model": "2_📥_Model.py",
-    "📋 Evaluation": "3_📋_Evaluation.py"
+    "📋 Evaluation": "2_📋_Evaluation.py",
+    "📋 Demo": "demo.py"
 }
 
 def set_page_config():
@@ -34,30 +35,9 @@ def display_title_and_description():
         """
     )
 
-def handle_devs_button():
-    # Center-align the button in the sidebar
-    if "show_additional_pages" not in st.session_state:
-        st.session_state.show_additional_pages = False
-
-    if st.sidebar.button("For Devs"):
-        st.session_state.show_additional_pages = not st.session_state.show_additional_pages
-
-    # Show the content of dataset.py when "For Devs" button is clicked and the section is expanded
-    if st.session_state.show_additional_pages:
-        st.sidebar.header("Datasets")
-        # Create a button or checkbox to load the dataset
-        if st.sidebar.button("Load Dataset"):
-            # Load and execute the content of dataset.py
-            current_dir = os.path.dirname(os.path.abspath(__file__))
-            dataset_path = os.path.join(current_dir, "devs", "dataset.py")
-            with open(dataset_path, "r") as file:
-                code = file.read()
-            exec(code)
-
 def main():
     set_page_config()
     display_title_and_description()
-    handle_devs_button()
 
 if __name__ == "__main__":
     main()
